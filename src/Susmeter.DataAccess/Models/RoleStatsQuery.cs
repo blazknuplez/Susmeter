7ef2b424480e3973c925c1f2bbf2124874dee6c7
BlazKnuplez <bknuplez@gmail.com>
2020-11-11 20:37:02 +0100

    updated shit. added AddPlayer form, added DeadliestImpostors, 5HeadCrewmates

---
+++ src/Susmeter.DataAccess/Models/RoleStatsQuery.cs
@@ -0,0 +1,13 @@
+﻿using Susmeter.Abstractions.Models;
+
+namespace Susmeter.DataAccess.Models
+{
+    internal class RoleStatsQuery
+    {
+        public long PlayerId { get; set; }
+
+        public string Nickname { get; set; }
+
+        public Role WinningRole { get; set; }
+    }
+}
